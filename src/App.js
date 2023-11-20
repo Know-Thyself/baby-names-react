5a75c7ee4c59617d31b10e0f6f39c38e9a942c5b
Biruk Kebede <biruk.key@gmail.com>
2023-11-20 01:07:54 +0000

    improved layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import './App.css';
-import babyNames from "./babyNamesData.json";
-import BoysAndGirlsNames from './BoysAndGirlsNames';
+import React from 'react'
+import './App.css'
+import babyNames from './babyNamesData.json'
+import BoysAndGirlsNames from './BoysAndGirlsNames'
 
 function App() {
-  return (
-    <div key="App-container" className="App">
-      <header key="App-header" className="App-header">
-        <h2 key="main-header" className="names-header">All About Awesome Baby Names!</h2>
-        <BoysAndGirlsNames babyNames={babyNames}/>
-      </header>
-    </div>
-  );
+	return (
+		<main key='main' className='main'>
+			<h2 key='main-header' className='names-header'>
+				All About Awesome Baby Names!
+			</h2>
+			<BoysAndGirlsNames babyNames={babyNames} />
+		</main>
+	)
 }
 
-export default App;
+export default App
--- src/BoysAndGirlsNames.js
+++ src/BoysAndGirlsNames.js
@@ -1,102 +1,144 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faX } from '@fortawesome/free-solid-svg-icons'
 
-const BoysAndGirlsNames = (props) => {
-    
-  const [babyNames, setBabyNames] = useState(props.babyNames);
-  const [searchInput, setSearchInput] = useState([]);
-  const [favorites, setFavorites] = useState([]);
-  const [gender, setGender] = useState("boys & girls")
-  const categories = ["boys & girls", "girls", "boys"];
-  
-  const sortedNames = babyNames.sort((a,b)=> {
-    let a1 = a.name.toLowerCase();
-    let b1 = b.name.toLowerCase();
-    return a1 < b1 ? -1: a1 > b1 ? 1:0;
-  }) 
+const BoysAndGirlsNames = props => {
+	const [babyNames, setBabyNames] = useState(props.babyNames)
+	const [searchInput, setSearchInput] = useState([])
+	const [favorites, setFavorites] = useState([])
+	const [gender, setGender] = useState('boys & girls')
+	const categories = ['boys & girls', 'girls', 'boys']
 
-  function handleSearchInput (e) {
-    setSearchInput(e.target.value.toLowerCase());
+	const sortedNames = babyNames.sort((a, b) => {
+		let a1 = a.name.toLowerCase()
+		let b1 = b.name.toLowerCase()
+		return a1 < b1 ? -1 : a1 > b1 ? 1 : 0
+	})
 
-    const filteredSearch = babyNames.filter(kid => {
-      return kid.name.toLowerCase().includes(searchInput);
-    });
-    setBabyNames(filteredSearch);
-    if (e.target.value === "") setBabyNames(props.babyNames);
-  }
-    
-  const favoritesListHandler = (id) => {
-    const newList = babyNames.filter((list) => {
-      return list.id !== id;
-    });
-    const favList = babyNames.filter((list) => {
-      return list.id === id;
-    })
-    setBabyNames(newList);
-    setFavorites((favorites) => favorites.concat(favList));
-  }
-    
-  const favoritesListRemover = (id) => {
-    const updatedList = favorites.filter((list) => list.id !== id);
-    setFavorites(updatedList);
-    const removed = favorites.filter((list) => list.id === id);
-    const merged = [...babyNames, ...removed];
+	function handleSearchInput(e) {
+		setSearchInput(e.target.value.toLowerCase())
 
-    const filteredList = merged.reduce((accumulator, current) => {
-      const x = accumulator.find(list => list.id === current.id);
-        if (!x) return accumulator.concat([current]);
-        else return accumulator;
-    }, []);
-    setBabyNames(filteredList);
-  }
+		const filteredSearch = babyNames.filter(kid => {
+			return kid.name.toLowerCase().includes(searchInput)
+		})
 
-  const boysCategory = props.babyNames.filter(baby => baby.sex === "m");
-  const girlsCategory = props.babyNames.filter(baby => baby.sex === "f");
+		setBabyNames(filteredSearch)
 
-  const RadioButtonsToggler = (e) => {
-    if (e.target.value === "boys") {
-      return setBabyNames(boysCategory);
-    }
-    else if (e.target.value === "girls") setBabyNames(girlsCategory);
-    else if (e.target.value === "boys & girls") setBabyNames(props.babyNames)
-  } 
+		if (e.target.value === '') setBabyNames(props.babyNames)
+	}
 
-  return (
-    <div key="baby-names-container" className="names">
-      <div key="favorites" className="favorite-names">
-        <h3 key="main-header" className="section-header">Click your favorite baby names to create your own collection</h3>
-        <ul>
-          {favorites && favorites.map(favorite =><li key={favorite.id} id={favorite.id} className={favorite.sex}><button onClick={() => favoritesListRemover(favorite.id)} className="favorite-button">{favorite.name}</button></li>)} 
-        </ul>
-      </div>
+	const favoritesListHandler = id => {
+		const newList = babyNames.filter(list => {
+			return list.id !== id
+		})
 
-      <div key="input-field" className="col">
-        <div key="input-form" className="search-input-wrapper">
-          <i class="fas fa-search"></i>
-          <input 
-            key="search-input "
-            type="text"
-            className="search-bar"
-            placeholder="Search for a baby name..."
-            value={searchInput}
-            onChange={handleSearchInput}
-          />
-        </div>
-  
-        <div key="radio" className="radio-container">{categories.map((category, index) => (
-          <>
-            <input className="radio-input" key={index} type="radio" value={category} name="selector" id={category} checked={gender === category} onChange={(e) => {setGender(e.target.value)}} onClick={RadioButtonsToggler}/><label key={category} htmlFor={category} className="radio-label">{category}</label>
-          </>
-          ))}
-        </div>
-      </div>
+		const favList = babyNames.filter(list => {
+			return list.id === id
+		})
 
-      <ul key="baby-names">
-        {babyNames && sortedNames.map(baby =>(
-        <li key={baby.id} id={baby.id} className={baby.sex}><button key={baby.id} id={baby.id} onClick={() => favoritesListHandler(baby.id)} className="favorite-button">{baby.name}</button></li>
-                ))}
-      </ul>
-    </div>
-  );
+		setBabyNames(newList)
+		setFavorites(favorites => favorites.concat(favList))
+	}
+
+	const favoritesListRemover = id => {
+		const updatedList = favorites.filter(list => list.id !== id)
+		setFavorites(updatedList)
+		const removed = favorites.filter(list => list.id === id)
+		const merged = [...babyNames, ...removed]
+
+		const filteredList = merged.reduce((accumulator, current) => {
+			const x = accumulator.find(list => list.id === current.id)
+			if (!x) return accumulator.concat([current])
+			else return accumulator
+		}, [])
+		setBabyNames(filteredList)
+	}
+
+	const boysCategory = props.babyNames.filter(baby => baby.sex === 'm')
+	const girlsCategory = props.babyNames.filter(baby => baby.sex === 'f')
+
+	const RadioButtonsToggler = e => {
+		if (e.target.value === 'boys') {
+			return setBabyNames(boysCategory)
+		} else if (e.target.value === 'girls') setBabyNames(girlsCategory)
+		else if (e.target.value === 'boys & girls') setBabyNames(props.babyNames)
+	}
+
+	return (
+		<div key='baby-names-container' className='names'>
+			<div key='favorites' className='favorite-names'>
+				<h3 key='main-header' className='section-header'>
+					Click your favorite baby names to create your own collection
+				</h3>
+				<div className='favorites-wrapper'>
+					{favorites &&
+						favorites.map(favorite => (
+							<div className='close-btn'>
+								<button
+									onClick={() => favoritesListRemover(favorite.id)}
+									className={`favorite-button ${favorite.sex}`}
+								>
+									{favorite.name}
+									<FontAwesomeIcon icon={faX} className='fa-x' />
+									<span></span>
+								</button>
+							</div>
+						))}
+				</div>
+			</div>
+
+			<div key='input-field' className='col'>
+				<div key='input-form' className='search-input-wrapper'>
+					<i class='fas fa-search'></i>
+					<input
+						key='search-input '
+						type='text'
+						className='search-bar'
+						placeholder='Search for a baby name...'
+						value={searchInput}
+						onChange={handleSearchInput}
+					/>
+				</div>
+
+				<div key='radio' className='radio-container'>
+					{categories.map((category, index) => (
+						<>
+							<input
+								className='radio-input'
+								key={index}
+								type='radio'
+								value={category}
+								name='selector'
+								id={category}
+								checked={gender === category}
+								onChange={e => {
+									setGender(e.target.value)
+								}}
+								onClick={RadioButtonsToggler}
+							/>
+							<label key={category} htmlFor={category} className='radio-label'>
+								{category}
+							</label>
+						</>
+					))}
+				</div>
+			</div>
+
+			<div key='baby-names' className='baby-names'>
+				{babyNames &&
+					sortedNames.map(baby => (
+						<button
+							key={baby.id}
+							id={baby.id}
+							onClick={() => favoritesListHandler(baby.id)}
+							className={`favorite-button ${baby.sex}`}
+						>
+							{baby.name}
+						</button>
+					))}
+			</div>
+		</div>
+	)
 }
 
-export default BoysAndGirlsNames;
+export default BoysAndGirlsNames
